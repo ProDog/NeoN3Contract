1689425b7c890fa01d22875ff5c259483f34db20
ProDog <gripzhang@outlook.com>
2021-05-13 14:12:55 +0800

    update Nep17Contract

--- Nep17Contract/Nep17Contract.cs
+++ Nep17Contract/Nep17Contract.cs
@@ -1,4 +1,5 @@
 ﻿using Neo;
+using Neo.SmartContract;
 using Neo.SmartContract.Framework;
 using Neo.SmartContract.Framework.Native;
 using Neo.SmartContract.Framework.Services;
@@ -15,13 +16,32 @@ namespace Neo3Contract
     [ContractPermission("*", "*")]
     public partial class Nep17Contract : Nep17Token
     {
-        [InitialValue("NUNtEBBbJkmPrmhiVSPN6JuM7AcE8FJ5sE", Neo.SmartContract.ContractParameterType.Hash160)]
+        [InitialValue("NUNtEBBbJkmPrmhiVSPN6JuM7AcE8FJ5sE", ContractParameterType.Hash160)]
         public static UInt160 Owner;
+
+        [InitialValue("f563ea40bc283d4d0e05c48ea305b3f2a07340ef", ContractParameterType.ByteArray)]
+        public static UInt160 ScriptHash;
 
         public delegate void Notify(params object[] arg);
 
         [DisplayName("test_event")]
         public static event Notify OnNotify;
+
+        protected const byte Prefix_Owner = 0x10;
+
+        public static void _deploy(object data, bool update)
+        {
+            if (update)
+            {
+                OnNotify("update", 1);
+            }
+            else
+            {
+                Mint(Owner, 1_0000_0000_00000000);                
+                Storage.Put(Storage.CurrentContext, new byte[] { Prefix_Owner }, Owner);
+                OnNotify("deploy", 1);
+            }
+        }
 
         public override string Symbol()
         {
@@ -38,36 +58,49 @@ namespace Neo3Contract
             return Runtime.CheckWitness(Owner);
         }
 
-        public static void _deploy(object data, bool update)
+        public static bool IsOwner()
         {
-            if (update)
-            {
-                Storage.Put(Storage.CurrentContext, "update", 11);
-                OnNotify("update", 1);
-            }
-            else
-            {
-                Mint(Owner, 1_0000_0000_00000000);
-                Storage.Put(Storage.CurrentContext, "deploy", 12345);
-                OnNotify("deploy", 1);
-            }
+            return Runtime.CheckWitness(GetOwner());
+        }
+
+        public static UInt160 GetOwner()
+        {
+            return (UInt160)Storage.Get(Storage.CurrentContext, new byte[] { Prefix_Owner });
+        }
+
+        public static bool SetOwner(UInt160 newOwner)
+        {
+            if (!IsOwner()) return false;
+            Storage.Put(Storage.CurrentContext, new byte[] { Prefix_Owner }, newOwner);
+            return true;
+        }
+
+        public static bool Mint(UInt160 to, BigInteger amt)
+        {
+            if (!IsOwner()) return false;
+            Nep17Token.Mint(to, amt);
+            return true;
+        }
+
+        public static bool Burn(UInt160 account, BigInteger amt)
+        {
+            if (!IsOwner()) return false;
+            Nep17Token.Burn(account, amt);
+            return true;
         }
 
         public static bool Update(ByteString nef, string manifest)
         {
-            if (!Runtime.CheckWitness(Owner)) return false;
+            if (!IsOwner()) return false;
             ContractManagement.Update(nef, manifest, null);
             return true;
         }
 
-        public static new void Mint(UInt160 to, BigInteger amt)
+        public static bool Destroy()
         {
-            Nep17Token.Mint(to, amt);
-        }
-
-        public static new void Burn(UInt160 account, BigInteger amt)
-        {
-            Nep17Token.Burn(account, amt);
+            if (!IsOwner()) return false;
+            ContractManagement.Destroy();
+            return true;
         }
 
         public static void OnNEP17Payment(UInt160 from, BigInteger amount, object data)
